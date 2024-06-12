1f5abf610540a931a34866fd83319bd09fa9701a
SabrinaMokhtari <s4mokhtari@uwaterloo.ca>
2024-06-12 16:20:00 +0000

    Update config.py

--- config.py
+++ config.py
@@ -8,24 +8,29 @@ use_cuda = torch.cuda.is_available()
 device = torch.device("cuda" if use_cuda else "cpu")
 
 params = {
-    'dataset' : 'eyepacs_complete',  # dataset name. Options: cifar10, chexpert, eyepacs_complete
+    # use tensors for when you are directly working with SN tensors
+    'dataset' : 'eyepacs_complete_tensors',  # dataset name. Options: cifar10, chexpert, eyepacs_complete, cifar10_tensors, chexpert_tensors, eyepacs_complete_tensors
     'minibatch_size' : 64, # mini batch size for dpsgd
     'max_physical_batch_size': 16,
     'aug_multiplicity' : False, # if augmentation multiplicity turned on
     'n_augs' : 8, # number of augmentations
-    'baseline' : 'wrn', # baseline model. Options: clip_g14, clip_g16, wrn, scatternet
+    'baseline' : 'scatternet', # baseline model. Options: clip_g14, clip_g16, wrn, scatternet
     'mode' : 'full', #wrn training mode. Options: 'full', 'final', 'scratch'
-    'model' : 'lr', # model. Options: lr, cnn, tlnn
+    'model' : 'cnn', # model. Options: lr, cnn, tlnn
     'epochs' : 20, # number of epochs
     'lr' : 1e-3,
+    'optim': 'Adam', # optimizer. Options: Adam, SGD
     'privacy' : True, # if privacy turned on
     'epsilon' : 1.0, # epsilon for privacy
     'clip_norm' : 1.0, # clip norm for privacy
     'reps' : 3, # number of repetitions for each experiment
     'ema_flag' : False, # if exponential parameter averaging turned on
     'norm_flag' : 'GroupNorm', # None, GroupNorm, DataNorm
-    'group_norm_groups' : 8, # number of groups for GroupNorm
+    'group_norm_groups' : 9, # number of groups for GroupNorm
     'data_norm_sigma' : 8.0, # sigma for DataNorm
+    'weight_standardization': False, # if weight standardization turned on
+    'grad_sample_mode': None, # None, 'no_op' for augmentation multiplicity
+    'use_scattering': False, # when not using scatter featurs directly and want to calculate them on the fly
     'device' : device, # device
 }
     
