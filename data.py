c0cbe56c756a8c246d525cac08b24e428c794fef
Shubhankar <s3mohapa@uwaterloo.ca>
2024-06-13 03:18:33 +0000

    updated data.py

--- data.py
+++ data.py
@@ -24,8 +24,9 @@ from torchvision import transforms
 
 # import webdataset as wds
 import json
+import config
 
-
+data_dir = config.DATA_DIR
 
 SHAPES = {
     "cifar10": (32, 32, 3),
@@ -37,34 +38,10 @@ SHAPES = {
     "chexpert": (224, 224, 3),
     "chexpert_single": (224, 224, 3),
     "eyepacs": (224, 224, 3),
-    "eyepacs_complete": (224, 224, 3),
 }
 
 def identity(x):
     return torch.tensor(json.loads(x))
-
-class MyImageDataset(Dataset):
-    def __init__(self, root, transform=None, target_transform=None, augment=None):
-        self.root = root
-        self.augment = augment
-        self.transform = transform
-        self.target_transform = target_transform
-        self.image_folder = torchvision.datasets.ImageFolder(self.root)
-
-    def __len__(self):
-        return len(self.image_folder.imgs)
-
-    def __getitem__(self, idx):
-        path, target = self.image_folder.imgs[idx]
-        img = self.image_folder.loader(path)
-        if self.transform is not None:
-            img = self.transform(img)
-        if self.target_transform is not None:
-            target = self.target_transform(target)
-        return img, target
-
-    def __getshape__(self):
-        return self.__len__(), *self.__getitem__(0)[0].shape
 
 
 class EyePACSDataset(Dataset):
@@ -330,53 +307,9 @@ def get_data(name, augment=False, **kwargs):
                                   transform=transforms.ToTensor(),
                                   download=True)
 
-    elif name == "cifar10_500K":
-
-        # extended version of CIFAR-10 with pseudo-labelled tinyimages
-
-        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                         std=[0.229, 0.224, 0.225])
-
-        if augment:
-            train_transforms = [
-                transforms.RandomHorizontalFlip(),
-                transforms.RandomCrop(32, 4),
-                transforms.ToTensor(),
-                normalize,
-            ]
-        else:
-            train_transforms = [
-                transforms.ToTensor(),
-                normalize,
-            ]
-
-        train_set = SemiSupervisedDataset(kwargs['aux_data_filename'],
-                                          root=".data",
-                                          train=True,
-                                          download=True,
-                                          transform=transforms.Compose(train_transforms))
-        test_set = None
-
-    elif name == "chest_xray":
-        main_dir = '/content/drive/MyDrive/CS860/Project/Kaggle/chest_xray'
-        train_dir = main_dir + '/train'
-        test_dir = main_dir + '/test'
-
-        trans = transforms.Compose(transforms=[transforms.Resize((64, 64)), transforms.ToTensor()])
-        train_set = MyImageDataset(root=train_dir, transform=trans)
-        test_set = MyImageDataset(root=test_dir, transform=trans)
-
-    elif name == "eye":
-        main_dir = '/content/drive/MyDrive/CS860/Project/Kaggle/OCT2017 '
-        train_dir = main_dir + '/train'
-        test_dir = main_dir + '/test'
-
-        trans = transforms.Compose(transforms=[transforms.Resize((64, 64)), transforms.ToTensor()])
-        train_set = MyImageDataset(root=train_dir, transform=trans)
-        test_set = MyImageDataset(root=test_dir, transform=trans)
 
     elif name == "chexpert":
-        root = '/share/TheSalon_Benchmarks/chexpertchestxrays-u20210408/CheXpert-v1.0/'
+        root = f'{data_dir}/raw/{name}/CheXpert-v1.0/'
 
         # Index: -1 denotes multi-label mode including 5 diseases
         train_set = CheXpert(csv_path=root + 'train.csv', image_root_path=root, use_upsampling=False, use_frontal=False,
@@ -384,31 +317,22 @@ def get_data(name, augment=False, **kwargs):
         test_set = CheXpert(csv_path=root + 'test.csv', image_root_path=root, use_upsampling=False, use_frontal=False,
                             image_size=224, mode='test', class_index=-1)
 
-    elif name == "chexpert_single":
-        root = '/share/TheSalon_Benchmarks/chexpertchestxrays-u20210408/CheXpert-v1.0/'
-
-        # Index: -1 denotes multi-label mode including 5 diseases
-        train_set = CheXpert(csv_path=root + 'train.csv', image_root_path=root, use_upsampling=True, use_frontal=True,
-                             image_size=224, mode='train', class_index=1)
-        test_set = CheXpert(csv_path=root + 'valid.csv', image_root_path=root, use_upsampling=False, use_frontal=True,
-                            image_size=224, mode='valid', class_index=1)
-
     elif name == "eyepacs":
         transform = transforms.Compose([transforms.Resize((224, 224)),
                                         transforms.ToTensor()])
 
-        train_set = EyePACSDataset('/shared/shared_1/eyepacs_complete/trainLabels.csv',
-                                 '/shared/shared_1/eyepacs_complete/train',
+        train_set = EyePACSDataset(f'{data_dir}/raw/{name}/trainLabels.csv',
+                                 f'{data_dir}/raw/{name}/train',
                                  transform=transform)
-        test_set = EyePACSDataset('/shared/shared_1/eyepacs_complete/retinopathy_solution.csv', 
-                                '/shared/shared_1/eyepacs_complete/test',
+        test_set = EyePACSDataset(f'{data_dir}/raw/{name}/retinopathy_solution.csv', 
+                                f'{data_dir}/raw/{name}/test',
                                 transform=transform)
 
     elif name == "eyepacs_tensors":
-        train_dir_scatters = f".data/{name}/train/scatters"
-        train_dir_targets = f".data/{name}/train/targets"
-        test_dir_scatters = f".data/{name}/test/scatters"
-        test_dir_targets = f".data/{name}/test/targets"
+        train_dir_scatters = f"{data_dir}/{name}/train/scatters"
+        train_dir_targets = f"{data_dir}/{name}/train/targets"
+        test_dir_scatters = f"{data_dir}/{name}/test/scatters"
+        test_dir_targets = f"{data_dir}/{name}/test/targets"
         train_set = ScatterDataset(
             x_folder=train_dir_scatters,
             y_folder=train_dir_targets)
@@ -416,53 +340,18 @@ def get_data(name, augment=False, **kwargs):
             x_folder=test_dir_scatters,
             y_folder=test_dir_targets)
     
-    elif name == "eyepacs_complete_tensors_augmented":
-        train_set = ScatterDataset(
-            x_folder='/u2/s4mokhta/embeddings/eyepacs_complete_tensors_8/train/scatters',
-            y_folder='/u2/s4mokhta/embeddings/eyepacs_complete_tensors_8/train/targets')
-        test_set = ScatterDataset(
-            x_folder='/u2/s4mokhta/embeddings/eyepacs_complete_tensors/test/scatters',
-            y_folder='/u2/s4mokhta/embeddings/eyepacs_complete_tensors/test/targets')
-
-    elif name == "chexpert_single_tensors":
-        train_set = ScatterDataset(
-            x_folder='/share/TheSalon_Benchmarks/embeddings/chexpert224_single/train/scatters',
-            y_folder='/share/TheSalon_Benchmarks/embeddings/chexpert224_single/train/targets')
-        test_set = ScatterDataset(
-            x_folder='/share/TheSalon_Benchmarks/embeddings/chexpert224_single/test/scatters',
-            y_folder='/share/TheSalon_Benchmarks/embeddings/chexpert224_single/test/targets')
 
     elif name == "chexpert_tensors":
-        train_dir_scatters = f".data/{name}/train/scatters"
-        train_dir_targets = f".data/{name}/train/targets"
-        test_dir_scatters = f".data/{name}/test/scatters"
-        test_dir_targets = f".data/{name}/test/targets"
+        train_dir_scatters = f"{data_dir}/{name}/train/scatters"
+        train_dir_targets = f"{data_dir}/{name}/train/targets"
+        test_dir_scatters = f"{data_dir}/{name}/test/scatters"
+        test_dir_targets = f"{data_dir}/{name}/test/targets"
         train_set = ScatterDataset(
             x_folder=train_dir_scatters,
             y_folder=train_dir_targets)
         test_set = ScatterDataset(
             x_folder=test_dir_scatters,
             y_folder=test_dir_targets)
-    
-    elif name == "chexpert_tensors_augmented":
-        train_set = ScatterDataset(
-            x_folder='/shared/shared_1/embeddings/chexpert_tensors_augmented_8/train/scatters',
-            y_folder='/shared/shared_1/embeddings/chexpert_tensors_augmented_8/train/targets')
-        test_set = ScatterDataset(
-            x_folder='/shared/shared_1/embeddings/chexlocalize/test/scatters',
-            y_folder='/shared/shared_1/embeddings/chexlocalize/test/targets')
-
-    elif name == "mnist_tensors":
-        train_set = ScatterDataset(x_folder='/home/s4mokhta/DP-Benchmarks/Handcrafted-DP/embeddings/MNIST/scatters',
-                                   y_folder='/home/s4mokhta/DP-Benchmarks/Handcrafted-DP/embeddings/MNIST/targets')
-        test_set = ScatterDataset(x_folder='/home/s4mokhta/DP-Benchmarks/Handcrafted-DP/embeddings/MNIST/scatters',
-                                  y_folder='/home/s4mokhta/DP-Benchmarks/Handcrafted-DP/embeddings/MNIST/targets')
-
-    elif name == "cifar10_tensors":
-        train_set = ScatterDataset(x_folder='/home/s4mokhta/DP-Benchmarks/Handcrafted-DP/embeddings/cifar10/scatters',
-                                   y_folder='/home/s4mokhta/DP-Benchmarks/Handcrafted-DP/embeddings/cifar10/targets')
-        test_set = ScatterDataset(x_folder='/home/s4mokhta/DP-Benchmarks/Handcrafted-DP/embeddings/cifar10/scatters',
-                                  y_folder='/home/s4mokhta/DP-Benchmarks/Handcrafted-DP/embeddings/cifar10/targets')
 
     else:
         raise ValueError(f"unknown dataset {name}")
@@ -470,154 +359,7 @@ def get_data(name, augment=False, **kwargs):
     return train_set, test_set
 
 
-class SemiSupervisedDataset(torch.utils.data.Dataset):
-    def __init__(self,
-                 aux_data_filename=None,
-                 train=False,
-                 **kwargs):
-        """A dataset with auxiliary pseudo-labeled data"""
 
-        self.dataset = datasets.CIFAR10(train=train, **kwargs)
-        self.train = train
-
-        # shuffle cifar-10
-        p = np.random.permutation(len(self.data))
-        self.data = self.data[p]
-        self.targets = list(np.asarray(self.targets)[p])
-
-        if self.train:
-            self.sup_indices = list(range(len(self.targets)))
-            self.unsup_indices = []
-
-            aux_path = os.path.join(kwargs['root'], aux_data_filename)
-            print("Loading data from %s" % aux_path)
-            with open(aux_path, 'rb') as f:
-                aux = pickle.load(f)
-            aux_data = aux['data']
-            aux_targets = aux['extrapolated_targets']
-            orig_len = len(self.data)
-
-            # shuffle additional data
-            p = np.random.permutation(len(aux_data))
-            aux_data = aux_data[p]
-            aux_targets = aux_targets[p]
-
-            self.data = np.concatenate((self.data, aux_data), axis=0)
-            self.targets.extend(aux_targets)
-
-            # note that we use unsup indices to track the labeled datapoints
-            # whose labels are "fake"
-            self.unsup_indices.extend(
-                range(orig_len, orig_len + len(aux_data)))
-
-            logger = logging.getLogger()
-            logger.info("Training set")
-            logger.info("Number of training samples: %d", len(self.targets))
-            logger.info("Number of supervised samples: %d",
-                        len(self.sup_indices))
-            logger.info("Number of unsup samples: %d", len(self.unsup_indices))
-            logger.info("Label (and pseudo-label) histogram: %s",
-                        tuple(
-                            zip(*np.unique(self.targets, return_counts=True))))
-            logger.info("Shape of training data: %s", np.shape(self.data))
-
-        # Test set
-        else:
-            self.sup_indices = list(range(len(self.targets)))
-            self.unsup_indices = []
-
-            logger = logging.getLogger()
-            logger.info("Test set")
-            logger.info("Number of samples: %d", len(self.targets))
-            logger.info("Label histogram: %s",
-                        tuple(
-                            zip(*np.unique(self.targets, return_counts=True))))
-            logger.info("Shape of data: %s", np.shape(self.data))
-
-    @property
-    def data(self):
-        return self.dataset.data
-
-    @data.setter
-    def data(self, value):
-        self.dataset.data = value
-
-    @property
-    def targets(self):
-        return self.dataset.targets
-
-    @targets.setter
-    def targets(self, value):
-        self.dataset.targets = value
-
-    def __len__(self):
-        return len(self.dataset)
-
-    def __getitem__(self, item):
-        self.dataset.labels = self.targets  # because torchvision is annoying
-        return self.dataset[item]
-
-    def __repr__(self):
-        fmt_str = 'Semisupervised Dataset ' + self.__class__.__name__ + '\n'
-        fmt_str += '    Number of datapoints: {}\n'.format(self.__len__())
-        fmt_str += '    Training: {}\n'.format(self.train)
-        fmt_str += '    Root Location: {}\n'.format(self.dataset.root)
-        tmp = '    Transforms (if any): '
-        fmt_str += '{0}{1}\n'.format(tmp, self.dataset.transform.__repr__().replace('\n', '\n' + ' ' * len(tmp)))
-        tmp = '    Target Transforms (if any): '
-        fmt_str += '{0}{1}'.format(tmp, self.dataset.target_transform.__repr__().replace('\n', '\n' + ' ' * len(tmp)))
-        return fmt_str
-
-
-class SemiSupervisedSampler(torch.utils.data.Sampler):
-    def __init__(self, num_examples, num_batches, batch_size):
-        self.inds = list(range(num_examples))
-        self.batch_size = batch_size
-        self.num_batches = num_batches
-        super().__init__(None)
-
-    def __iter__(self):
-        batch_counter = 0
-        inds_shuffled = [self.inds[i] for i in torch.randperm(len(self.inds))]
-
-        while len(inds_shuffled) < self.num_batches * self.batch_size:
-            temp = [self.inds[i] for i in torch.randperm(len(self.inds))]
-            inds_shuffled.extend(temp)
-
-        for k in range(0, self.num_batches * self.batch_size, self.batch_size):
-            if batch_counter == self.num_batches:
-                break
-
-            batch = inds_shuffled[k:(k + self.batch_size)]
-
-            # this shuffle operation is very important, without it
-            # batch-norm / DataParallel hell ensues
-            np.random.shuffle(batch)
-            yield batch
-            batch_counter += 1
-
-    def __len__(self):
-        return self.num_batches
-
-
-class PoissonSampler(torch.utils.data.Sampler):
-    def __init__(self, num_examples, batch_size):
-        self.inds = np.arange(num_examples)
-        self.batch_size = batch_size
-        self.num_batches = int(np.ceil(num_examples / batch_size))
-        self.sample_rate = self.batch_size / (1.0 * num_examples)
-        super().__init__(None)
-
-    def __iter__(self):
-        # select each data point independently with probability `sample_rate`
-        for i in range(self.num_batches):
-            batch_idxs = np.random.binomial(n=1, p=self.sample_rate, size=len(self.inds))
-            batch = self.inds[batch_idxs.astype(np.bool)]
-            np.random.shuffle(batch)
-            yield batch
-
-    def __len__(self):
-        return self.num_batches
 
 
 def get_scattered_dataset(loader, scattering, device, data_size):
@@ -683,7 +425,7 @@ def get_scattered_loader(loader, scattering, device, drop_last=False, sample_bat
         # for tensor in data:
         print("Data Shape: ", data.shape)
 
-        data_file_name = '/shared/shared_1/embeddings/chexpert_tensors_augmented_8/train/scatters/scatters' + str(scatter_counter) + '.pt'
+        data_file_name = f'{data_dir}/embeddings/chexpert_tensors_augmented_8/train/scatters/scatters' + str(scatter_counter) + '.pt'
 
         data_cloned = data.clone()
 
@@ -696,7 +438,7 @@ def get_scattered_loader(loader, scattering, device, drop_last=False, sample_bat
 
         for t in target:
             print("Targets Shape: ", t.shape)
-            target_file_name = '/shared/shared_1/embeddings/chexpert_tensors_augmented_8/train/targets/targets' + str(target_counter) + '.pt'
+            target_file_name = f'{data_dir}/embeddings/chexpert_tensors_augmented_8/train/targets/targets' + str(target_counter) + '.pt'
 
             target_cloned = t.clone()
 
@@ -742,10 +484,10 @@ def create_scattered_features(loader, scattering, device, aug_multiplicity=False
     scatter_counter = 0
     target_counter = 0
 
-    data_path = f".data/{dataset}_tensors/{data_type}/scatters"
-    target_path = f".data/{dataset}_tensors/{data_type}/targets"
-    if not os.path.exists(data_path):
-        os.makedirs(data_path)
+    train_path = f".data/{dataset}/{data_type}/scatters"
+    target_path = f".data/{dataset}/{data_type}/targets"
+    if not os.path.exists(train_path):
+        os.makedirs(train_path)
     if not os.path.exists(target_path):
         os.makedirs(target_path)
 
